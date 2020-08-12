7f829d1c499faa8218261c09adb9aa936bdf1654
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-12 11:25:02 -0500

    Update obj_dy_val.js

--- obj_dy_val.js
+++ obj_dy_val.js
@@ -20,3 +20,31 @@ function afficherProps(obj, nomObjet) {
   return resultat;
 }
 afficherProps(maVoiture, "maVoiture");
+
+//version 2.0, dynamically provides values to the objet
+var modele = ["VW","MERCEDES","AUDI","BMW","MERCEDES"];
+var maVoiture = new Object();
+var j = 0;
+for(let i = 0; i < modele.length; i++){
+  j = i;
+  maVoiture[modele[i]] = modele[j];
+}
+//maVoiture.fabricant = "VW PASSAT TDI";
+//maVoiture.modèle = "Mustang";
+//maVoiture.année = 1969;
+
+//var nomPropriété = "fabricant";
+//maVoiture[nomPropriété] = "Ford";
+
+//nomPropriété = "modèle";
+//maVoiture[nomPropriété] = "Mustang";
+function afficherProps(obj, nomObjet) {
+  var resultat = "";
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+        resultat += nomObjet + "." + i + " = " + obj[i] + "\n";
+    }
+  }
+  return resultat;
+}
+afficherProps(maVoiture, "maVoiture");
